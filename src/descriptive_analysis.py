7f1b0649cdec246bfa91d2020cede3363342e3e7
coni26 <56028648+coni26@users.noreply.github.com>
2020-12-08 00:05:15 +0100

    Create descriptive_analysis.py

---
+++ src/descriptive_analysis.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from ast import literal_eval
+from nltk.corpus import stopwords
+import networkx as nx
+
+from nltk.stem.snowball import FrenchStemmer
+stemmer = FrenchStemmer()
