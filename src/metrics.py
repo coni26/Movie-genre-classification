e349ccaca1527e6f5af60a930c6eebe3b49bd2b0
victorpvrd <73067125+victorpvrd@users.noreply.github.com>
2020-12-13 12:42:55 +0100

    Creation metrics

---
+++ src/metrics.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def precision(y_pred, y):
+    res = 0
+    if len(y.shape)==1:
+        for i in range(len(y)):
+            res += y_pred[i]*y[i]
+        res/=max(sum(y_pred),1)
+    else:
+        n = y.shape[1]
+        for i in range(len(y)):
+            res +=  sum(abs(y_pred[i,:]*y[i,:]))/max(sum(y_pred[i,:]),1)
+        res/=len(y)
+    return res
+
+
+def recall(y_pred, y):
+    res = 0
+    if len(y.shape)==1:
+        for i in range(len(y)):
+            res += y_pred[i]*y[i]
+        res/=sum(y)
+    else:
+        n = y.shape[1]
+        for i in range(len(y)):
+            res += sum(abs(y_pred[i,:]*y[i,:]))/max(sum(y[i,:]),1)
+        res/=len(y)
+    return res
+
+
+def F1_score(y_pred, y):
+    rec = recall(y_pred, y)
+    prec = precision(y_pred, y)
+    return 2*(rec*prec)/(rec+prec)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
