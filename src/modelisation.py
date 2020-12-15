39afc547aadda0660258169fb79e2cbcdd40b109
victorpvrd <73067125+victorpvrd@users.noreply.github.com>
2020-12-15 12:03:33 +0100

    Update modelisation.py

--- src/modelisation.py
+++ src/modelisation.py
@@ -20,9 +20,7 @@ def create_hash(df):
     l_hash = [None]*4096
     index = 0
     l_words = []
-    
     banned_words = ban_words(df)
-
     for word in banned_words:
         if len(word)>2:
             buf = compute_hash(word)
@@ -30,8 +28,6 @@ def create_hash(df):
                 l_hash[buf] = {}
             if not(word in l_hash[buf]):
                 l_hash[buf][word] = None
-
-
     for i, row in df.iterrows():
         l = row['Synopsis']
         for word in l:
@@ -43,7 +39,6 @@ def create_hash(df):
                     l_hash[buf][word] = index
                     l_words.append(word)
                     index+=1
-                    
     return l_hash, index
 
 
@@ -52,7 +47,6 @@ def create_tfidf(df):
     mat = np.zeros((len(df), index), dtype=int)
     l_presence = [0]*index
     l_len = [0]*len(df)
-
     for i, row in df.iterrows():
         l = row['Synopsis']
         for k, word in enumerate(l):
@@ -66,7 +60,6 @@ def create_tfidf(df):
                         l_presence[ind]+=1
     mat_tfidf = np.zeros((len(df), index))
     n = len(df)
-
     for i, row in df.iterrows():
         l = row['Synopsis']
         for k, word in enumerate(l):
@@ -75,16 +68,13 @@ def create_tfidf(df):
                 ind = l_hash[buf][word]
                 if ind!=None:
                     mat_tfidf[i, ind] = mat[i, ind] / l_len[i] * math.log(n/l_presence[ind])
-    
     return mat_tfidf, l_hash, index
 
 
 def create_tfidf_test(df, l_hash, index):
-    
     mat = np.zeros((len(df), index), dtype=int)
     l_presence = [0]*index
     l_len = [0]*len(df)
-
     for i, row in df.iterrows():
         l = row['Synopsis']
         for k, word in enumerate(l):
@@ -99,7 +89,6 @@ def create_tfidf_test(df, l_hash, index):
                             l_presence[ind]+=1
     mat_tfidf = np.zeros((len(df), index))
     n = len(df)
-
     for i, row in df.iterrows():
         l = row['Synopsis']
         for k, word in enumerate(l):
@@ -109,8 +98,8 @@ def create_tfidf_test(df, l_hash, index):
                     ind = l_hash[buf][word]
                     if ind!=None:
                         mat_tfidf[i, ind] = mat[i, ind] / l_len[i] * math.log(n/l_presence[ind])
-    
     return mat_tfidf
+
 
 def y_mat(df):
     y = np.zeros((len(df), len(dic_genres)), dtype=int)
